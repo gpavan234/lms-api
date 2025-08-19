6769250331117baa1da0d1d63b852081838dbbd1
gpavan234 <gpavan234@gmail.com>
2025-08-19 19:43:08 +0530

    feat: Setup MongoDB connection and User model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,25 @@
-const getUsers = (req, res) => {
-  res.json([
-    { id: 1, name: "John Doe", role: "student" },
-    { id: 2, name: "Jane Smith", role: "instructor" }
-  ]);
+import User from  '../models/user.js';
+
+// GET all users
+export const getUsers = async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
 };
 
-module.exports = { getUsers };
+// CREATE a user (dummy)
+export const createUser = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+
+  const user = await User.create({ name, email, password, role });
+
+  if (user) {
+    res.status(201).json(user);
+  } else {
+    res.status(400).json({ message: 'Invalid user data' });
+  }
+};
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
+import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-
 dotenv.config();
+connectDB();
+
 const app = express();
 
-//midedlewares
-app.use(morgan('dev'))
-app.use(cors());
+// Middleware
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 // Routes
 app.use('/api/users', userRoutes);
@@ -29,5 +31,5 @@ app.use(errorHandler);
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
