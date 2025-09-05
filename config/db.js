5318e65f602e8c19baef7ada9b727eb263ad4fcb
gpavan234 <gpavan234@gmail.com>
2025-09-05 19:42:42 +0530

    update the db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // 5 sec
-    });
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ Error: ${error.message}`);
-    process.exit(1); // Exit process if failed
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
